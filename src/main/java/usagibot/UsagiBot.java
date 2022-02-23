37fa32ecbd06c175cbb0f5db9836d62c368e50bb
Scoin0 <john.anfernee.lewis@gmail.com>
2022-02-22 16:51:28 -1000

    Initial Commit

---
+++ src/main/java/usagibot/UsagiBot.java
@@ -0,0 +1,19 @@
+package usagibot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UsagiBot {
+
+    static final Logger log = LoggerFactory.getLogger(UsagiBot.class);
+
+    public static void main(String[] args) {
+        log.info("Hello World.");
+    }
+
+    public static void testing() {
+        log.info("Testing.");
+        d
+    }
+
+}
---
+++ src/main/java/usagibot/configuration/Config.java
@@ -0,0 +1,4 @@
+package usagibot.configuration;
+
+public class Config {
+}
