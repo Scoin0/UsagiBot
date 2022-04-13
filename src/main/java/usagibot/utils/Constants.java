cc4109588ec33faf01dbb9545551f78530f0b5b4
Scoin0 <john.anfernee.lewis@gmail.com>
2022-04-12 20:37:11 -1000

    misc

---
+++ src/main/java/usagibot/utils/Constants.java
@@ -0,0 +1,14 @@
+package usagibot.utils;
+
+import usagibot.utils.version.Version;
+import usagibot.utils.version.VersionUtil;
+
+public class Constants {
+
+    public static final Version version = VersionUtil.getCurrentVersion();
+    public static final String stats = "<username> (USER ID) " +
+            "URL" +
+            "Rank: #" +
+            "Preformance: # (PP)" +
+            "Total Score: ";
+}
