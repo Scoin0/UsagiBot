63df1c822fd7aee414c4fbb9ece22d4bfff344ae
Scoin0 <john.anfernee.lewis@gmail.com>
2022-04-12 22:00:35 -1000

    Obtains beatmaps from urls now

--- src/main/java/usagibot/twitch/event/ChatEvent.java
+++ src/main/java/usagibot/twitch/event/ChatEvent.java
@@ -5,8 +5,12 @@ import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
 import com.github.twitch4j.helix.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import usagibot.UsagiBot;
+import usagibot.osu.objects.Beatmap;
 import usagibot.twitch.TwitchClient;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Slf4j
@@ -14,6 +18,7 @@ public class ChatEvent {
 
     private final String prefix = UsagiBot.getConfig().getPrefix();
     private final String channel = UsagiBot.getConfig().getTwitchChannel();
+    private Beatmap beatmap;
 
     public ChatEvent(SimpleEventHandler eventHandler) {
         eventHandler.onEvent(ChannelMessageEvent.class, this::onChannelMessage);
@@ -29,9 +34,11 @@ public class ChatEvent {
         }
 
         if (event.getMessage().contains("https://osu.ppy.sh")) {
-            String[] message = event.getMessage().split("#osu/");
-            log.info(message.length + "" + message[1]);
-            //beatmap = UsagiBot.getClient().getBeatmap("2965016");
+            log.info("Received possible osu song request. Parsing now...");
+            beatmap = UsagiBot.getClient().getBeatmap(parseMessage(event.getMessage()));
+            log.info("Beatmap ID Found: " + beatmap.getId());
+            sendMessage("[Received] " + beatmap.getBeatmapset().getTitle() + " - " + beatmap.getBeatmapset().getArtist() +
+                    " [" + beatmap.getVersion() + "] " + beatmap.getDifficulty_rating() + "⭐");
             //UsagiBot.getIrcBot().getUserChannelDao().getUser("I_Only_Hit_100s").send().message("Map Received - " +
                     //"http://osu.ppy.sh/b/" + beatmap.getId());
         }
@@ -46,6 +53,29 @@ public class ChatEvent {
         TwitchClient.client.getChat().sendMessage(channel, message);
     }
 
-    public void parseMessage(String message) {
+    // Grabs the map digits
+    public String parseMessage(String message) {
+        String delimiters = "https?:\\/\\/osu.ppy.sh\\/(b|s|beatmapsets)\\/([0-9]*)(#osu|#taiko|#ctb|#maina)\\/([0-9]*)";
+        Pattern urlPattern = Pattern.compile(delimiters, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+        Matcher matcher = urlPattern.matcher(message);
+        List<String> urlSplit = new ArrayList<String>();
+
+        while (matcher.find()) {
+            for (int i = 1; i <= matcher.groupCount(); i++) {
+                urlSplit.add(matcher.group(i));
+            }
+        }
+
+        switch (urlSplit.size()) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                String beatmap = urlSplit.get(3);
+                return beatmap;
+            default:
+                log.warn("Scoin0 has not yet implemented all ways to request a beatmap. Only Beatmap Sets work right now.");
+        }
+        return null;
     }
 }
