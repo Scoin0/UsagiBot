9b88b4bd31a01c2556da625440a0029e3dc25a97
Scoin0 <john.anfernee.lewis@gmail.com>
2022-04-17 20:02:30 -1000

    Removed unnecessary code

--- src/main/java/usagibot/osu/irc/OsuListener.java
+++ src/main/java/usagibot/osu/irc/OsuListener.java
@@ -1,24 +1,17 @@
 package usagibot.osu.irc;
 
+import kotlin.Suppress;
 import lombok.extern.slf4j.Slf4j;
 import org.pircbotx.InputParser;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.*;
-import usagibot.UsagiBot;
-import usagibot.osu.objects.Beatmap;
 
 @Slf4j
 public class OsuListener extends ListenerAdapter {
 
-    Beatmap beatmap;
-    boolean slient = true;
-
     @Override
     public void onConnect(ConnectEvent event) throws Exception {
-        log.info("I have connected.");
-        beatmap = UsagiBot.getClient().getBeatmap("3206350");
-        String link = "[https://osu.ppy.sh/beatmapsets/" + beatmap.getBeatmapset_id() + "#osu/" + beatmap.getId() + " " + beatmap.getBeatmapset().getTitle() + " - " + beatmap.getBeatmapset().getArtist() + "] [" + beatmap.getVersion() + "]";
-        UsagiBot.getIrcBot().getUserChannelDao().getUser("I_Only_Hit_100s").send().message(link);
+        log.info("I have connected to BanchoIRC");
     }
 
     @Override
