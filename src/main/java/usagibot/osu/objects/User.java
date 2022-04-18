f0e4497ed0ee1d74007ee2aeb2d8f66b1f0a0ad8
Scoin0 <john.anfernee.lewis@gmail.com>
2022-04-17 17:18:42 -1000

    Added the User and UserCompact objects

--- src/main/java/usagibot/osu/objects/User.java
+++ src/main/java/usagibot/osu/objects/User.java
@@ -1,4 +1,52 @@
 package usagibot.osu.objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Timestamp;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Slf4j
 public class User extends UserCompact {
+
+    // Represents a User.
+    @JsonProperty("cover.url")
+    private String cover_url;
+    @JsonProperty("discord")
+    private String[] discord;
+    @JsonProperty("has_supported")
+    private boolean has_supported;
+    @JsonProperty("interests")
+    private String[] interests;
+    @JsonProperty("join_date")
+    private Timestamp join_date;
+    @JsonProperty("kudosu.avaliable")
+    private int kudosu_avaliable;
+    @JsonProperty("kudosu.total")
+    private int kudosu_total;
+    @JsonProperty("location")
+    private String[] location;
+    @JsonProperty("max_blocks")
+    private int max_blocks;
+    @JsonProperty("max_friends")
+    private int max_friends;
+    @JsonProperty("occupation")
+    private String[] occupation;
+    @JsonProperty("playmode")
+    private GameMode playmode;
+    @JsonProperty("playstyle")
+    private String[] playstyle;
+    @JsonProperty("post_count")
+    private int post_count;
+    @JsonProperty("title")
+    private String[] title;
+    @JsonProperty("title_url")
+    private String[] title_url;
+    @JsonProperty("twitter")
+    private String[] twitter;
+    @JsonProperty("website")
+    private String[] website;
 }
--- src/main/java/usagibot/osu/objects/UserCompact.java
+++ src/main/java/usagibot/osu/objects/UserCompact.java
@@ -38,14 +38,101 @@ public class UserCompact {
     @JsonProperty("username")
     private String username;
 
-    // Optional attributes
+    // Optional attributes | Why does half this page not have a type?
+    @JsonProperty("account_history")
+    private AccountUserHistory[] account_history;
+    @JsonProperty("active_tournament_banner")
+    private ProfileBanner active_tournament_banner;
+    @JsonProperty("badges")
+    private UserBadge[] badges;
     @JsonProperty("beatmap_playcount_count")
     private int beatmap_playcount_count;
     @JsonProperty("favourite_beatmapset_count")
     private int favourite_beatmapset_count;
     @JsonProperty("follower_count")
     private int follower_count;
+    @JsonProperty("graveyard_beatmapset_count")
+    private int graveyard_beatmapset_count;
     @JsonProperty("is_restricted")
     private boolean is_restricted;
+    @JsonProperty("loved_beatmapset_count")
+    private int loved_beatmapset_count;
+    @JsonProperty("scores_best_count")
+    private int scores_best_count;
+    @JsonProperty("scores_first_count")
+    private int scores_first_count;
+    @JsonProperty("scores_recent_count")
+    private int scores_recent_count;
 
+    public static class ProfileBanner {
+
+        private int id;
+        private int tournament_id;
+        private String image;
+
+        public int getId() {
+            return id;
+        }
+
+        public int getTournament_Id() {
+            return tournament_id;
+        }
+
+        public String getImage() {
+            return image;
+        }
+    }
+
+    public static class AccountUserHistory {
+
+        private String[] description;
+        private int id;
+        private int length;
+        private Timestamp timestamp;
+        private String type;
+
+        public String[] getDescription() {
+            return description;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        public Timestamp getTimestamp() {
+            return timestamp;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
+
+    public static class UserBadge {
+
+        private Timestamp timestamp;
+        private String description;
+        private String image_url;
+        private String url;
+
+        public Timestamp getTimestamp() {
+            return timestamp;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getImage_url() {
+            return image_url;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+    }
 }
