3919250e02b69e847425a6d229dd75a1dcd0cb89
Scoin0 <john.anfernee.lewis@gmail.com>
2022-04-17 14:02:11 -1000

    Preparing to remove this class for the Utility class

--- src/main/java/usagibot/utils/Constants.java
+++ src/main/java/usagibot/utils/Constants.java
@@ -6,9 +6,4 @@ import usagibot.utils.version.VersionUtil;
 public class Constants {
 
     public static final Version version = VersionUtil.getCurrentVersion();
-    public static final String stats = "<username> (USER ID) " +
-            "URL" +
-            "Rank: #" +
-            "Preformance: # (PP)" +
-            "Total Score: ";
 }
