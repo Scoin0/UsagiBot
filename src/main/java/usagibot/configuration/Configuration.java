964e14cf5e5add83c4e179203e2b833ba598b32d
Scoin0 <john.anfernee.lewis@gmail.com>
2022-05-25 21:49:51 -1000

    Removed formatting in Config file

--- src/main/java/usagibot/configuration/Configuration.java
+++ src/main/java/usagibot/configuration/Configuration.java
@@ -1,13 +1,17 @@
 package usagibot.configuration;
 
+import com.github.twitch4j.common.events.domain.EventUser;
 import lombok.*;
+import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import usagibot.osu.objects.Beatmap;
+import usagibot.utils.Utility;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,7 +19,6 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Data
 @Getter
 @Setter
 @NoArgsConstructor
@@ -26,26 +29,30 @@ public class Configuration {
 
     // Global Settings
     private String prefix = "!";
-    private int configVersion = 1;
+    private int configVersion = 2;
     private boolean useUpdater = true;
 
     // Twitch Settings
-    private String twitchUsername;
-    private String twitchPassword;
-    private String twitchChannel;
-    private String twitchMessage;
+    private String twitchUsername = "";
+    private String twitchPassword = "";
+    private String twitchChannel = "";
 
     // GOsu Settings
-    private String gOsuUrlPath;
+    private String gOsuUrlPath = "http://127.0.0.1:24050/json";
 
     // Osu Settings
-    private String banchoUsername;
-    private String banchoPassword;
-    private String banchoServer;
-    private int banchoPort;
-    private String banchoChannel;
-    private String osuClientId;
-    private String osuAPIKey;
+    private String banchoUsername = "";
+    private String banchoPassword = "";
+    private String banchoServer = "irc.ppy.sh";
+    private int banchoPort = 6667;
+    private String banchoChannel = "#osu";
+    private String osuClientId = "";
+    private String osuAPIKey = "";
+
+    // Custom Messages
+    private String twitchMessage = "[RECEIVED] > <user_sent> [<ranked_status>] <artist> - <title> [<version>] <music_note_emoji> <length> <star_emoji> <star_rating> BPM:<bpm> AR:<ar> OD:<od>";
+    private String osuIrcMessage = "[<user_sent>] > [https://osu.ppy.sh/beatmapsets/<beatmap_id> <artist> - <title> [<version>]] <music_note_emoji> <length> <star_emoji> <star_rating> BPM:<bpm> AR:<ar> OD:<od>";
+    private String nowPlayingMessage = "Here you go! <beatmap_url>";
 
     public void createConfiguration() {
 
@@ -53,10 +60,7 @@ public class Configuration {
             if (!file.exists()) {
                 log.info("The config file has not been found. Generating one now...");
                 file.createNewFile();
-
-                @Cleanup
-                FileWriter fileWriter = new FileWriter(file);
-                fileWriter.write(generateConfiguration());
+                generateConfiguration();
             }
         } catch (Exception e) {
             log.error(e.getMessage());
@@ -66,36 +70,36 @@ public class Configuration {
     public void loadConfiguration() throws Exception {
         PropertiesConfiguration config = new PropertiesConfiguration(file);
         FileInputStream fileInput = new FileInputStream(file);
-        config.load();
         fileInput.close();
 
-        Map<String, Object> keywords = new HashMap<>();
-        keywords.put("username", twitchUsername);
-        keywords.put("musicnote", "\u266B");
-        keywords.put("star", "\u2605");
-
+        // Load the configuration first
         log.info("Loading Config...");
+        prefix = config.getString("prefix");
+        useUpdater = config.getBoolean("update");
+        twitchUsername = config.getString("twitch_username");
+        twitchPassword = config.getString("twitch_password");
+        twitchChannel = config.getString("twitch_channel");
+        gOsuUrlPath = config.getString("gosu_url_path");
+        osuClientId = config.getString("osu_api_clientid");
+        osuAPIKey = config.getString("osu_api_key");
+        banchoUsername = config.getString("bancho_username");
+        banchoPassword = config.getString("bancho_password");
+        banchoServer = config.getString("bancho_server");
+        banchoPort = config.getInt("bancho_port");
+        banchoChannel = config.getString("bancho_channel");
+        twitchMessage = config.getString("received_message");
+        osuIrcMessage = config.getString("osu_message");
+        nowPlayingMessage = config.getString("np_message");
+
+        // Check to see if the config version is different from my version.
         if (configVersion != config.getInt("configVersion")) {
             log.info("There has been an update to the Config file.");
-            log.info("Merging old data to new Config file...");
-            // TODO: Add updating config
+            log.info("Adding new values...");
+            file.delete();
+            generateConfiguration();
             log.info("Completed. Reloading Config...");
+            initConfiguration();
         } else {
-            prefix = config.getString("prefix");
-            useUpdater = config.getBoolean("update");
-            twitchUsername = config.getString("twitch_username");
-            twitchPassword = config.getString("twitch_password");
-            twitchChannel = config.getString("twitch_channel");
-            gOsuUrlPath = config.getString("gosu_url_path");
-            osuClientId = config.getString("osu_api_clientid");
-            osuAPIKey = config.getString("osu_api_key");
-            banchoUsername = config.getString("bancho_username");
-            banchoPassword = config.getString("bancho_password");
-            banchoServer = config.getString("bancho_server");
-            banchoPort = config.getInt("bancho_port");
-            banchoChannel = config.getString("bancho_channel");
-
-            twitchMessage = parseKeywords(config.getString("twitch_message"), keywords);
             log.info("Config Loaded.");
         }
     }
@@ -106,6 +110,65 @@ public class Configuration {
         } else {
             createConfiguration();
         }
+    }
+
+    // TODO: Clean
+    public String getOsuIrcMessage(Beatmap beatmap, EventUser user) {
+        Map<String, Object> keywords = new HashMap<>();
+        keywords.put("music_note_emoji", "\u266B");
+        keywords.put("star_emoji", "\u2605");
+        keywords.put("ranked_status", beatmap.getStatus());
+        keywords.put("artist", beatmap.getBeatmapset().getArtist());
+        keywords.put("title", beatmap.getBeatmapset().getTitle());
+        keywords.put("version", beatmap.getVersion());
+        keywords.put("length", beatmap.getTotal_length());
+        keywords.put("star_rating", beatmap.getDifficulty_rating());
+        keywords.put("beatmap_id", beatmap.getBeatmapset_id());
+        keywords.put("beatmap_url", beatmap.getUrl());
+        keywords.put("bpm", beatmap.getBpm());
+        keywords.put("ar", beatmap.getAr());
+        keywords.put("od", beatmap.getDrain());
+        keywords.put("user_sent", user.getName());
+        return parseKeywords(osuIrcMessage, keywords);
+    }
+
+    // TODO: Clean
+    public String getTwitchMessage(Beatmap beatmap, EventUser user) {
+        Map<String, Object> keywords = new HashMap<>();
+        keywords.put("music_note_emoji", "\u266B");
+        keywords.put("star_emoji", "\u2605");
+        keywords.put("ranked_status", beatmap.getStatus());
+        keywords.put("artist", beatmap.getBeatmapset().getArtist());
+        keywords.put("title", beatmap.getBeatmapset().getTitle());
+        keywords.put("version", beatmap.getVersion());
+        keywords.put("length", beatmap.getTotal_length());
+        keywords.put("star_rating", beatmap.getDifficulty_rating());
+        keywords.put("beatmap_id", beatmap.getBeatmapset_id());
+        keywords.put("beatmap_url", beatmap.getUrl());
+        keywords.put("bpm", beatmap.getBpm());
+        keywords.put("ar", beatmap.getAr());
+        keywords.put("od", beatmap.getDrain());
+        keywords.put("user_sent", user.getName());
+        return parseKeywords(twitchMessage, keywords);
+    }
+
+    public String getNowPlayingMessage(EventUser user) throws IOException {
+        Map<String, Object> keywords = new HashMap<>();
+        keywords.put("music_note_emoji", "\u266B");
+        keywords.put("star_emoji", "\u2605");
+        keywords.put("ranked_status", Utility.getSongFromGosuMemory().getStatus());
+        keywords.put("artist", Utility.getSongFromGosuMemory().getBeatmapset().getArtist());
+        keywords.put("title", Utility.getSongFromGosuMemory().getBeatmapset().getTitle());
+        keywords.put("version", Utility.getSongFromGosuMemory().getVersion());
+        keywords.put("length", Utility.getSongFromGosuMemory().getTotal_length());
+        keywords.put("star_rating", Utility.getSongFromGosuMemory().getDifficulty_rating());
+        keywords.put("beatmap_id", Utility.getSongFromGosuMemory().getBeatmapset_id());
+        keywords.put("beatmap_url", Utility.getSongFromGosuMemory().getUrl());
+        keywords.put("bpm", Utility.getSongFromGosuMemory().getBpm());
+        keywords.put("ar", Utility.getSongFromGosuMemory().getAr());
+        keywords.put("od", Utility.getSongFromGosuMemory().getDrain());
+        keywords.put("user_sent", user.getName());
+        return parseKeywords(nowPlayingMessage, keywords);
     }
 
     public String parseKeywords(String message, Map<String, Object> keywords) {
@@ -127,105 +190,27 @@ public class Configuration {
         return String.format(formatter.toString(), keywordsList.toArray());
     }
 
-    public static String generateConfiguration() {
-        return "############################################################\n" +
-                "# +------------------------------------------------------+ #\n" +
-                "# |                      Usagibot                        | #\n" +
-                "# |                 Configuration File                   | #\n" +
-                "# +------------------------------------------------------+ #\n" +
-                "############################################################\n" +
-                "\n" +
-                "# DO NOT TOUCH\n" +
-                "configVersion = 1\n" +
-                "\n" +
-                "############################################################\n" +
-                "# +------------------------------------------------------+ #\n" +
-                "# |                 Settings (Global)                    | #\n" +
-                "# +------------------------------------------------------+ #\n" +
-                "############################################################\n" +
-                "\n" +
-                "# The prefix for the Twitch bot\n" +
-                "# Default is !\n" +
-                "prefix = !\n" +
-                "\n" +
-                "# This program is capable to automatically update finding a new version.\n" +
-                "# If true, the program will search for new updates and automatically download them.\n" +
-                "# If false, the program will not search for new updates. \n" +
-                "# Default is true\n" +
-                "update = true\n" +
-                "\n" +
-                "############################################################\n" +
-                "# +------------------------------------------------------+ #\n" +
-                "# |                  Twitch Settings                     | #\n" +
-                "# +------------------------------------------------------+ #\n" +
-                "############################################################\n" +
-                "\n" +
-                "# The Twitch bot IRC username and password (required)\n" +
-                "#\n" +
-                "# See https://twitchapps.com/tmi/ for\n" +
-                "# help obtaining a Twitch IRC token\n" +
-                "#\n" +
-                "# Example: \n" +
-                "#   twitch_username = UsagiBot\n" +
-                "#   twitch_password = oauth:abc123\n" +
-                "twitch_username =\n" +
-                "twitch_password =\n" +
-                "\n" +
-                "# Twitch channel name (required)\n" +
-                "#\n" +
-                "# This might be different if you are running the bot on another Twitch account.\n" +
-                "# Change this to the channel you are streaming on.\n" +
-                "#\n" +
-                "# Example:\n" +
-                "#   twitch_channel = Scoin0\n" +
-                "twitch_channel = \n" +
-                "twitch_message = \n" +
-                "\n" +
-                "############################################################\n" +
-                "# +------------------------------------------------------+ #\n" +
-                "# |                   GOsu Settings                      | #\n" +
-                "# +------------------------------------------------------+ #\n" +
-                "############################################################\n" +
-                "\n" +
-                "# GOsuMemory websocket path (required if you want the !np command to work)\n" +
-                "#\n" +
-                "# Inside the config.ini for GOsuMemory, copy the link from serverip and paste as the gosu_url_path.\n" +
-                "#\n" +
-                "# Example:\n" +
-                "#   gosu_url_path = http://127.0.0.1:24050/json\n" +
-                "#\n" +
-                "# Default is http://127.0.0.1:24050/json\n" +
-                "gosu_url_path = http://127.0.0.1:24050/json\n" +
-                "\n" +
-                "############################################################\n" +
-                "# +------------------------------------------------------+ #\n" +
-                "# |                    Osu Settings                      | #\n" +
-                "# +------------------------------------------------------+ #\n" +
-                "############################################################\n" +
-                "\n" +
-                "# Osu! API key (required)\n" +
-                "#\n" +
-                "# See https://osu.ppy.sh/home/account/edit and scroll down to obtain an osu! API key\n" +
-                "#\n" +
-                "# Example:\n" +
-                "#   osu_api_clientid = 12345\n" +
-                "#   osu_api_key = abc123\n" +
-                "osu_api_clientid = \n" +
-                "osu_api_key = \n" +
-                "\n" +
-                "# Bancho API key IRC username, password,  (required)\n" +
-                "#\n" +
-                "# See https://osu.ppy.sh/p/irc on how to get a Bancho IRC password\n" +
-                "#\n" +
-                "# Example:\n" +
-                "#   bancho_username = UsagiBot\n" +
-                "#   bancho_password = abc123\n" +
-                "#   bancho_server = irc.ppy.sh\n" +
-                "#   bancho_port = 6667\n" +
-                "bancho_username = \n" +
-                "bancho_password = \n" +
-                "bancho_server = irc.ppy.sh\n" +
-                "bancho_port = 6667\n" +
-                "bancho_channel = #osu";
+    public void generateConfiguration() throws ConfigurationException {
+        PropertiesConfiguration config = new PropertiesConfiguration(file);
+        config.clear();
+        config.setHeader("Welcome to the Configuration File!\nPlease look at https://github.com/Scoin0/UsagiBot/wiki/Configuration to understand how to get all the api keys.");
+        config.addProperty("configVersion", configVersion);
+        config.addProperty("prefix", prefix);
+        config.addProperty("update", useUpdater);
+        config.addProperty("twitch_username", twitchUsername);
+        config.addProperty("twitch_password", twitchPassword);
+        config.addProperty("twitch_channel", twitchChannel);
+        config.addProperty("gosu_url_path", gOsuUrlPath);
+        config.addProperty("osu_api_clientid", osuClientId);
+        config.addProperty("osu_api_key", osuAPIKey);
+        config.addProperty("bancho_username", banchoUsername);
+        config.addProperty("bancho_password", banchoPassword);
+        config.addProperty("bancho_server", banchoServer);
+        config.addProperty("bancho_port", banchoPort);
+        config.addProperty("bancho_channel", banchoChannel);
+        config.addProperty("received_message", "[RECEIVED] > <user_sent> [<ranked_status>] <artist> - <title> [<version>] <music_note_emoji> <length> <star_emoji> <star_rating> BPM:<bpm> AR:<ar> OD:<od>");
+        config.addProperty("osu_message", "[<user_sent>] > [https://osu.ppy.sh/beatmapsets/<beatmap_id> <artist> - <title> [<version>]] <music_note_emoji> <length> <star_emoji> <star_rating> BPM:<bpm> AR:<ar> OD:<od>");
+        config.addProperty("np_message", "Here you go! <beatmap_url>");
+        config.save(file);
     }
 }
