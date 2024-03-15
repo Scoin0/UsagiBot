c3105ad6d2da7b148cba350682d8b98ade9ecb40
Scoin0 <john.anfernee.lewis@gmail.com>
2024-03-14 22:07:03 -1000

    Add new support for tosu

--- src/main/java/usagibot/osu/memreaders/tosu/TOsuReader.java
+++ src/main/java/usagibot/osu/memreaders/tosu/TOsuReader.java
@@ -1,7 +1,13 @@
 package usagibot.osu.memreaders.tosu;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import usagibot.UsagiBot;
 import usagibot.osu.api.Beatmap;
 import usagibot.osu.memreaders.IMemoryReader;
+import usagibot.osu.memreaders.MemoryReaderConnections;
+import usagibot.osu.memreaders.gosu.GOsuModel;
+
+import java.io.IOException;
 
 public class TOsuReader implements IMemoryReader {
 
@@ -9,11 +15,32 @@ public class TOsuReader implements IMemoryReader {
 
     @Override
     public Beatmap getSong() {
-        return null;
+        try {
+            String json = MemoryReaderConnections.fetchJsonData(MemoryReaderConnections.webHookPath);
+            ObjectMapper mapper = new ObjectMapper();
+            TOsuModel model = mapper.readValue(json, TOsuModel.class);
+            String beatmapId = String.valueOf(model.menu.beatmap.id);
+            return beatmap = UsagiBot.getClient().getBeatmap(beatmapId);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public String getMods() {
-        return null;
+        try {
+            String json = MemoryReaderConnections.fetchJsonData(MemoryReaderConnections.webHookPath);
+            ObjectMapper mapper = new ObjectMapper();
+            TOsuModel model = mapper.readValue(json, TOsuModel.class);
+            String mods = String.valueOf(model.menu.mods.str);
+            if (mods == "") { // Fix for tosu
+                return "NM";
+            }
+            return mods.replaceAll("^\"|\"$", "");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
